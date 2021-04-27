fdc148a1b6296057a9cee93e9171b3a4d4f1315b
Ngireddy <somulanagireddy@gmail.com>
2021-04-27 20:59:45 +0530

    [Nagireddy]: addedusing arrow function

--- JS_EmployeeWage/EmployeeWage.js
+++ JS_EmployeeWage/EmployeeWage.js
@@ -106,3 +106,24 @@ console.log("UC7G - Number of Days Emp Worked : "+empWageArray.reduce(totalDaysW
 //UC 8 - Calculate employee wage with employee wage map
 console.log(empDailyWageMap);
 console.log("UC8 - Emp Wage Map totalHrs : "+Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+//UC 9 - Arrow Functions
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+let count = 0;
+let totalHours = Array.from(empDailyWageMap.values()).reduce(findTotal, 0);
+let totalSalary = empWageArray.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+console.log("UC9A - Emp Wage with Arrow : " + " Total Hours : " + totalHours + " Total Wage : " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value, key, map) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days : " + fullWorkingDays);
+console.log("Part Working Days : " + partWorkingDays);
+console.log("Non Working Days : " + nonWorkingDays);
