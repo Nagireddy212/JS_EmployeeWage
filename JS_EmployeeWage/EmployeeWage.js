3a99fb13956f85779e85121f9796fea67760d3de
Ngireddy <somulanagireddy@gmail.com>
2021-04-27 20:29:00 +0530

    [Nagireddy]: addedclaculating Employee Wage

--- JS_EmployeeWage/EmployeeWage.js
+++ JS_EmployeeWage/EmployeeWage.js
@@ -12,34 +12,26 @@ if (empCheck == IS_ABSENT)
 }
 }
 
-//UC4
+//UC2
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20;
 
-function getWorkingHours(empCheck)
-{
-    switch (empCheck)
-    {
-        case IS_PART_TIME:
-             return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-             return FULL_TIME_HOURS;
-         default:
-             return 0;
-    
-    }
-}
 let empHrs = 0;
-
-for (let day = 0; day< NUM_OF_WORKING_DAYS; day++)
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck)
 {
-    let empCheck =Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);    
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
 }
 
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hours: "+ empHrs +"   Emp Wage: "+empWage);
+console.log(" Emp Wage: " + empWage);
